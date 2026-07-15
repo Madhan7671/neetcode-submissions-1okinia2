2b6fd3c38f9157728ce7b8726ed40d5e47e74094
MadhanMaddasani <118039222+Madhan7671@users.noreply.github.com>
2026-07-15 19:58:41 +0530

    Add: partition-to-k-equal-sum-subsets - submission-4

---
+++ Data Structures & Algorithms/partition-to-k-equal-sum-subsets/submission-4.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+bool fn(int index,vector<int>& nums,vector<int>&sides,int target,int k)
+{
+    if(index==nums.size())
+    {
+        for(int i=0;i<k;i++)
+        {
+            if(sides[i]!=target)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    for(int i=0;i<k;i++)
+    {
+        if(sides[i]+nums[index]>target)
+        {
+            continue;
+        }
+        sides[i]+=nums[index];
+        if(fn(index+1,nums,sides,target,k)){
+            return true;
+        }
+        sides[i]-=nums[index];
+    }
+    return false;
+}
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        if(nums.size()==0)
+        {
+            return true;
+        }
+        int sum=0;
+        for(int i:nums)
+        {
+            sum+=i;
+        }
+        if(sum%k!=0)
+        {
+            return false;
+        }
+        sort(nums.rbegin(),nums.rend());
+        vector<int>sides(k,0);
+        return(fn(0,nums,sides,sum/k,k));
+    }
+};
